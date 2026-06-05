151d7f134bb0da8c638bb66269a4ae4bc43be1a0
Szymon Malinowski <szmalinowski2014@gmail.com>
2026-06-05 15:39:44 +0200

    Add placeholder to input field in SearchBar component

--- src/components/search.bar.jsx
+++ src/components/search.bar.jsx
@@ -4,8 +4,10 @@ export default function SearchBar({ searchTerm, onChange }) {
       <div>
         <label htmlFor="seach-input">Search:</label>
         <input
+          className="input"
           id="search-input"
           type="text"
+          placeholder="word"
           value={searchTerm}
           onChange={(event) => onChange(event.target.value)}
         />

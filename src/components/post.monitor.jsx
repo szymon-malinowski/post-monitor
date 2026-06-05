d3068b8a98e2333a11ba5ca7992219d8d5f10f78
Szymon Malinowski <szmalinowski2014@gmail.com>
2026-06-05 08:58:18 +0200

    Add title update functionality to PostMonitor component based on search term

--- src/components/post.monitor.jsx
+++ src/components/post.monitor.jsx
@@ -3,6 +3,14 @@ import React, { useState, useEffect } from "react";
 export default function PostMonitor() {
   const [searchTerm, setSearchTerm] = useState("");
 
+  const defaultTitle = "Post Monitor";
+  useEffect(() => {
+    if (searchTerm.trim === "") {
+      document.title = defaultTitle;
+    } else {
+      document.title = searchTerm;
+    }
+  }, [searchTerm]);
   return (
     <>
       <h1>Post Monitor</h1>
